836432a3d54cdfd3363bf59c1338a98717468ee6
MilesZhao <mileszhao1990@yahoo.com>
2017-10-17 12:06:53 -0400

    ll

--- backup.php
+++ backup.php
@@ -1,4 +1,5 @@
 <?php
+phpinfo();
 
 echo "<h1>PDO demo!</h1>";
 
@@ -9,29 +10,6 @@ try {
 	    $conn = new PDO("mysql:host=$hostname;dbname=yz746",
 	    $username, $password);
 	    echo "Connected successfully"; 
-
-	    // $date = date('Y-m-d', time());
-	    // $sql ="INSERT INTO accounts (email,fname,lname,phone,birthday,gender,password)
-    	// VALUES ('john@example.com','John', 'Doe',911,'$date','M','1234')";
-    	// $q = $conn->prepare($sql);
-    	// $q->execute();
-
-	    // $sql = "select id,email from accounts";
-	    // $q = $conn->prepare($sql);
-	    // $q->execute();
-	    // $results = $q->fetchAll();
-
-	    // if($q->rowCount()){
-
-	    // 	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th></tr>";
-	    // 	foreach ($results as $row) {
-     //    		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
-    	// 	}
-	    // }else{
-	    // 	echo '0 results';
-	    // }
-
-
 
     }
 catch(PDOException $e)
