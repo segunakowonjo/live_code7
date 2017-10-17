0ea497e0ab6013ad91061385a33d4f8060a69799
MilesZhao <mileszhao1990@yahoo.com>
2017-10-17 12:09:18 -0400

    ll

--- backup.php
+++ backup.php
@@ -5,7 +5,7 @@ echo "<h1>PDO demo!</h1>";
 
 $hostname = "sql.njit.edu";
 $username = "yz746";
-$password = "Q5vvA0U5";
+$password = "Q5vvA0U";
 try {
 	    $conn = new PDO("mysql:host=$hostname;dbname=yz746",
 	    $username, $password);
