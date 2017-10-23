9c92b6410e77013486e2f838720f3f1c9594ece5
Your Name <segunakowonjo@yahoo.com>
2017-10-23 15:06:39 -0400

    Addin in the File

--- backup.php
+++ backup.php
@@ -9,18 +9,19 @@ try {
 	    $conn = new PDO("mysql:host=$hostname;dbname=ssa39",
 	    $username, $password);
 	    echo "Connected successfully<br>"; 
-	    $sql = "select id, email from accounts";
+	    $sql = "select * from accounts where id < 6";
 	    $q = $conn->prepare($sql);
 	    $q->execute();
 	    $results = $q->fetchAll();
 	    
 
 	    if($q->rowCount()){
+	    	echo count($results);
 
 
-	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th></tr>";
+	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
 	    	foreach ($results as $row) {
-        		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
+        		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
     		}
 	    }else{
 	    	echo '0 results';
