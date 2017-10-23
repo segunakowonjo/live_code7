4aa48d110fa10f9f81bba17516f4fc202ffc65e6
Your Name <segunakowonjo@yahoo.com>
2017-10-22 22:23:01 -0400

    I gain Alot

--- backup.php
+++ backup.php
@@ -2,13 +2,29 @@
 
 echo "<h1>PDO demo!</h1>";
 
-$hostname = "sql.njit.edu";
-$username = "yz746";
-$password = "Q5vvA0U";
+$hostname = "sql2.njit.edu";
+$username = "ssa39";
+$password = "PWiV2bts";
 try {
-	    $conn = new PDO("mysql:host=$hostname;dbname=yz746",
+	    $conn = new PDO("mysql:host=$hostname;dbname=ssa39",
 	    $username, $password);
-	    echo "Connected successfully"; 
+	    echo "Connected successfully<br>"; 
+	    $sql = "select id, email from accounts";
+	    $q = $conn->prepare($sql);
+	    $q->execute();
+	    $results = $q->fetchAll();
+	    
+
+	    if($q->rowCount()){
+
+
+	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th></tr>";
+	    	foreach ($results as $row) {
+        		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
+    		}
+	    }else{
+	    	echo '0 results';
+	    } 
 
     }
 catch(PDOException $e)
